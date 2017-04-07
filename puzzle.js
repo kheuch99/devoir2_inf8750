51dc025f5e67b6073ed581c93ed178527aa2575d
kheuch99 <cheikhbara99@gmail.com>
2017-04-06 20:19:48 -0400

    1. Ajout de la fonction d'hachage hashPrePuzzleKey. 2. Refactoring + comments.

--- puzzle.js
+++ puzzle.js
@@ -1,41 +1,69 @@
 var aesjs = require('aes-js');
 var _ = require('underscore');
+var sha256 = require('sha256');
 
+// 128-bit key (16 bytes * 8 bits/byte = 128 bits)
+const SIZE_OF_RANDOM_STR = 16;
+
+/*
+Cette fonction génère aléatoirement une chaine de caractère de longueur "length".
+Dans ce cas ci, la chaine de caractère est générée de sorte à mesurer 128 bits.
+@param: length, la longueur de la chaine de caractère à générer
+@return: str, la chaine de caractère aléatoire
+*/
 function randomString(length) {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var str = "";
+    var range = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for(var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+        str += range.charAt(Math.floor(Math.random() * range.length));
     }
-    return text;
 
+    return str;
 }
 
 /*
-Alice génère aléatoirement un ensemble de n paires
-(pre_puzzle_key(i),secret_key(i)) où i est l'index de cette paire (pour i entre 1 et n)
-et pre_puzzle_key(i) et secret_key(i) sont des chaînes aléatoires de 128 bits.
+Cette fonction génère une paire pre_puzzle_key & secret_key accompagnée de son index.
+@param: _index, l'index attribué à la paire générée
+@return: un objet contenant la paire pre_puzzle_key & secret_key générée ainsi que l'index qui va avec
 */
+function genPair(_index){
+    var pre_puzzle_key = randomString(SIZE_OF_RANDOM_STR);
+    var secret_key = randomString(SIZE_OF_RANDOM_STR);
 
-
-
-function operation_one(Index){
-    var pre_puzzle_key = randomString(16);
-    var secret_key = randomString(16);
     return {
-            pre_puzzle_key : pre_puzzle_key,
-            secret_key : secret_key,
-            index : Index
+        pre_puzzle_key : pre_puzzle_key,
+        secret_key : secret_key,
+        index : _index
     }
 }
 
-function generation_n_paire(N){
-    var table = new Array();
+/*
+Cette fonction génère N paires de pre_puzzle_key & secret_key accompagnée de leur index.
+@param: N, le nombre de paires à générer
+@return: un tableau contenant les paires pre_puzzle_key & secret_key générée ainsi que leur index
+*/
+function genPairs(N){
+    var tab = new Array();
     for(i = 0; i < N ; i++){
-        table.push(operation_one(i));
+        tab.push(genPair(i));
     }
-    return table;
+
+    return tab;
 }
 
-console.log(generation_n_paire(5))
+
+/*
+Cette fonction applique récursivement la fonction de hachage SHA-1 sur le "pre_puzzle_key" et ce "nbTimesToHash" fois.
+@param: nbTimesToHash, le nombre de fois que la fonction SHA-1 doit être appliqué
+@param: pre_puzzle_key, la chaine de caractère à hacher récursivement
+@return: puzzle_key = SHA-1 ^ 1000(pre_puzzle_key)
+*/
+function hashPrePuzzleKey(nbTimesToHash, pre_puzzle_key){
+
+    if(nbTimesToHash == 0){
+        return arguments[1];
+    }else{
+        return hashPrePuzzleKey(--nbTimesToHash, sha256(pre_puzzle_key));
+    }
+}
 
